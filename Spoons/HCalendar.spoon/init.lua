c079c9f523d0462f600a00ad154c8b7b2b46e916
dax <dax26d@gmail.com>
2025-08-17 11:15:08 +0800

    用stylua格式化代码 将星期数改为中文 新建了几个变量，试图显示三个月的日期

--- Spoons/HCalendar.spoon/init.lua
+++ Spoons/HCalendar.spoon/init.lua
@@ -3,8 +3,7 @@
 --- A horizonal calendar inset into the desktop
 ---
 --- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/HCalendar.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/HCalendar.spoon.zip)
-
-local obj={}
+local obj = {}
 obj.__index = obj
 
 -- Metadata
@@ -14,7 +13,7 @@ obj.author = "ashfinal <ashfinal@gmail.com>"
 obj.homepage = "https://github.com/Hammerspoon/Spoons"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
-obj.hcalw = 31*24+20
+obj.hcalw = 31 * 24 + 20
 obj.hcalh = 100
 
 local function updateHcalCanvas()
@@ -23,51 +22,86 @@ local function updateHcalCanvas()
     local currentyear = os.date("%Y")
     local currentmonth = os.date("%m")
     local currentday = os.date("%d")
-    local weeknames = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"}
-    local firstdayofnextmonth = os.time{year=currentyear, month=currentmonth+1, day=1}
-    local lastdayofcurrentmonth = os.date("*t", firstdayofnextmonth-24*60*60).day
-    for i=1,lastdayofcurrentmonth do
-        local weekdayofqueriedday = os.date("*t", os.time{year=currentyear, month=currentmonth, day=i}).wday
+    local nextmonth = (currentmonth + 1) % 12
+    local lastmonth = currentmonth == 1 and 12 or currentmonth - 1
+    local nextnextmonth = (nextmonth + 1) % 12
+    -- local weeknames = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"}
+    local weeknames = {"日", "一", "二", "三", "四", "五", "六"}
+    local firstdayofnextmonth = os.time({
+        year = currentmonth == 12 and currentyear + 1 or currentyear,
+        month = currentmonth == 12 and 1 or currentmonth + 1,
+        day = 1
+    })
+    local firstdayofcurrentmonth = os.time({
+        year = currentyear,
+        month = currentmonth,
+        day = 1
+    })
+    local firstdayofnextnextmonth = os.time({
+        year = nextmonth == 12 and currentyear + 1 or currentyear,
+        month = nextmonth == 12 and 1 or nextmonth + 1,
+        day = 1
+    })
+    local lastdayofcurrentmonth = os.date("*t",
+                                          firstdayofnextmonth - 24 * 60 * 60)
+                                      .day
+    local lastdayoflastmonth = os.date("*t",
+                                       firstdayofcurrentmonth - 24 * 60 * 60)
+                                   .day
+    local lastdayofnextmonth = os.date("*t",
+                                       firstdayofnextnextmonth - 24 * 60 * 60)
+                                   .day
+    local lastdays = {
+        lastdayoflastmonth, lastdayofcurrentmonth, lastdayofnextmonth
+    }
+    for i = 1, lastdayofcurrentmonth do
+        local weekdayofqueriedday = os.date("*t", os.time(
+                                                {
+                year = currentyear,
+                month = currentmonth,
+                day = i
+            })).wday
         local mappedweekdaystr = weeknames[weekdayofqueriedday]
-        obj.canvas[3+i].text = mappedweekdaystr
-        obj.canvas[65+i].text = i
-        if mappedweekdaystr == "Sa" or mappedweekdaystr == "Su" then
-            obj.canvas[3+i].textColor = {hex="#FF7878"}
-            obj.canvas[34+i].fillColor = {hex="#FF7878"}
-            obj.canvas[65+i].textColor = {hex="#FF7878"}
+        obj.canvas[3 + i].text = mappedweekdaystr
+        obj.canvas[65 + i].text = i
+        if mappedweekdaystr == "六" or mappedweekdaystr == "日" then
+            obj.canvas[3 + i].textColor = {hex = "#FF7878"}
+            obj.canvas[34 + i].fillColor = {hex = "#FF7878"}
+            obj.canvas[65 + i].textColor = {hex = "#FF7878"}
         else
             -- Restore the default colors
-            obj.canvas[3+i].textColor = {hex="#FFFFFF"}
-            obj.canvas[34+i].fillColor = {hex="#FFFFFF", alpha=0.5}
-            obj.canvas[65+i].textColor = {hex="#FFFFFF"}
+            obj.canvas[3 + i].textColor = {hex = "#FFFFFF"}
+            obj.canvas[34 + i].fillColor = {hex = "#FFFFFF", alpha = 0.5}
+            obj.canvas[65 + i].textColor = {hex = "#FFFFFF"}
         end
         if i == math.tointeger(currentday) then
-            obj.canvas[34+i].fillColor = {hex="#00BAFF", alpha=0.8}
-            obj.canvas[97].frame.x = tostring((10+24*(i-1))/obj.hcalw)
+            obj.canvas[34 + i].fillColor = {hex = "#00BAFF", alpha = 0.8}
+            obj.canvas[97].frame.x = tostring((10 + 24 * (i - 1)) / obj.hcalw)
         end
     end
     -- hide extra day
-    for i=lastdayofcurrentmonth+1, 31 do
-        obj.canvas[3+i].text = ""
-        obj.canvas[34+i].fillColor.alpha = 0
-        obj.canvas[65+i].text = ""
+    for i = lastdayofcurrentmonth + 1, 31 do
+        obj.canvas[3 + i].text = ""
+        obj.canvas[34 + i].fillColor.alpha = 0
+        obj.canvas[65 + i].text = ""
     end
     -- Adjust the size of clipmask to clip the canvas
-    obj.canvas[2].frame.w = tostring((lastdayofcurrentmonth*24+20)/obj.hcalw)
+    obj.canvas[2].frame.w = tostring((lastdayofcurrentmonth * 24 + 20) /
+                                         obj.hcalw)
 end
 
 function obj:init()
-    local hcalbgcolor = {hex="#000000", alpha=0.3}
-    local hcaltitlecolor = {hex="#FFFFFF", alpha=0.3}
-    local todaycolor = {hex="#FFFFFF", alpha=0.2}
-    local midlinecolor = {hex="#FFFFFF", alpha=0.5}
+    local hcalbgcolor = {hex = "#000000", alpha = 0.3}
+    local hcaltitlecolor = {hex = "#FFFFFF", alpha = 0.3}
+    local todaycolor = {hex = "#FFFFFF", alpha = 0.2}
+    local midlinecolor = {hex = "#FFFFFF", alpha = 0.5}
     local cscreen = hs.screen.mainScreen()
     local cres = cscreen:fullFrame()
     obj.canvas = hs.canvas.new({
         x = 40,
-        y = cres.h-obj.hcalh-40,
+        y = cres.h - obj.hcalh - 40,
         w = obj.hcalw,
-        h = obj.hcalh,
+        h = obj.hcalh
     }):show()
 
     obj.canvas:behavior(hs.canvas.windowBehaviors.canJoinAllSpaces)
@@ -77,7 +111,7 @@ function obj:init()
     obj.canvas[1] = {
         type = "rectangle",
         action = "clip",
-        roundedRectRadii = {xRadius = 10, yRadius = 10},
+        roundedRectRadii = {xRadius = 10, yRadius = 10}
     }
 
     -- *NOW* we can draw our actual "visible" parts
@@ -86,7 +120,7 @@ function obj:init()
         type = "rectangle",
         action = "fill",
         fillColor = hcalbgcolor,
-        roundedRectRadii = {xRadius = 10, yRadius = 10},
+        roundedRectRadii = {xRadius = 10, yRadius = 10}
     }
 
     obj.canvas[3] = {
@@ -97,15 +131,15 @@ function obj:init()
         textColor = hcaltitlecolor,
         textAlignment = "left",
         frame = {
-            x = tostring(10/obj.hcalw),
-            y = tostring(10/obj.hcalh),
-            w = tostring(1-20/obj.hcalw),
+            x = tostring(10 / obj.hcalw),
+            y = tostring(10 / obj.hcalh),
+            w = tostring(1 - 20 / obj.hcalw),
             h = "30%"
         }
     }
 
     -- upside weekday string
-    for i=4, 4+30 do
+    for i = 4, 4 + 30 do
         obj.canvas[i] = {
             type = "text",
             text = "",
@@ -113,31 +147,31 @@ function obj:init()
             textSize = 13,
             textAlignment = "center",
             frame = {
-                x = tostring((10+24*(i-4))/obj.hcalw),
+                x = tostring((10 + 24 * (i - 4)) / obj.hcalw),
                 y = "45%",
-                w = tostring(24/(obj.hcalw-20)),
+                w = tostring(24 / (obj.hcalw - 20)),
                 h = "23%"
             }
         }
     end
 
     -- midline rectangle
-    for i=35, 35+30 do
+    for i = 35, 35 + 30 do
         obj.canvas[i] = {
             type = "rectangle",
             action = "fill",
             fillColor = midlinecolor,
             frame = {
-                x = tostring((10+24*(i-35))/obj.hcalw),
+                x = tostring((10 + 24 * (i - 35)) / obj.hcalw),
                 y = "65%",
-                w = tostring(24/(obj.hcalw-20)),
+                w = tostring(24 / (obj.hcalw - 20)),
                 h = "4%"
             }
         }
     end
 
     -- downside day string
-    for i=66, 66+30 do
+    for i = 66, 66 + 30 do
         obj.canvas[i] = {
             type = "text",
             text = "",
@@ -145,9 +179,9 @@ function obj:init()
             textSize = 13,
             textAlignment = "center",
             frame = {
-                x = tostring((10+24*(i-66))/obj.hcalw),
+                x = tostring((10 + 24 * (i - 66)) / obj.hcalw),
                 y = "70%",
-                w = tostring(24/(obj.hcalw-20)),
+                w = tostring(24 / (obj.hcalw - 20)),
                 h = "23%"
             }
         }
@@ -160,9 +194,9 @@ function obj:init()
         fillColor = todaycolor,
         roundedRectRadii = {xRadius = 3, yRadius = 3},
         frame = {
-            x = tostring(10/obj.hcalw),
+            x = tostring(10 / obj.hcalw),
             y = "44%",
-            w = tostring(24/(obj.hcalw-20)),
+            w = tostring(24 / (obj.hcalw - 20)),
             h = "46%"
         }
     }
@@ -173,7 +207,6 @@ function obj:init()
     else
         obj.timer:start()
     end
-
 end
 
 return obj
